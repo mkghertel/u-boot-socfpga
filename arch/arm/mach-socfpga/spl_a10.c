1af62795820c07f8353c1d4b44b5d75041babfd3
Christian Thaler <christian.thaler@tes-dst.com>
2020-07-06 13:53:01 +0200

    add option to always configure FPGA after reboot
    This option is nice to have when a HW hang up occurs that can't be fixed by
    a simple reset.

--- arch/arm/mach-socfpga/spl_a10.c
+++ arch/arm/mach-socfpga/spl_a10.c
@@ -121,6 +121,7 @@ void spl_board_init(void)
 	arch_early_init_r();
 
 	/* If the full FPGA is already loaded, ie.from EPCQ, config fpga pins */
+#if !defined(CONFIG_SPL_FPGA_ALWAYS_CONFIG)
 	if (is_fpgamgr_user_mode()) {
 		ret = config_pins(gd->fdt_blob, "shared");
 
@@ -130,7 +131,9 @@ void spl_board_init(void)
 		ret = config_pins(gd->fdt_blob, "fpga");
 		if (ret)
 			return;
-	} else if (!is_fpgamgr_early_user_mode()) {
+	} else if (!is_fpgamgr_early_user_mode())
+#endif
+	{
 		/* Program IOSSM(early IO release) or full FPGA */
 		fpgamgr_program(buf, FPGA_BUFSIZ, 0);
 	}
